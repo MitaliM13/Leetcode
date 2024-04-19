12bab914688016d6fbd9e82421ef911803b4b814
Mitali Maurya <113610349+MitaliM13@users.noreply.github.com>
2024-04-19 10:22:09 +0530

    Time: 2 ms (99.65%) | Memory: 49.3 MB (66.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,43 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int ans=0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    bfs(grid,i,j,visited);
-                    ans++;
+        int islands = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return ans;
+        return islands;
     }
-    class Pair{
-        int first;
-        int second;
-        Pair(int first, int second){
-            this.first = first;
-            this.second = second;
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
         }
-    }
-    void bfs(char[][] grid,int i,int j,boolean[][] visited){
-        Queue<Pair> q = new LinkedList<Pair>();
-        visited[i][j]=true;
-        q.add(new Pair(i,j));
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        while(!q.isEmpty()){
-            int r = q.peek().first;
-            int c = q.peek().second;
-            q.remove();
-            for(int k=0;k<4;k++){
-                int rowNum = r + dx[k];
-                int colNum = c + dy[k];
-                if(0<=rowNum && rowNum<grid.length && colNum>=0 && colNum<grid[0].length && grid[rowNum][colNum]=='1' && visited[rowNum][colNum]==false){
-                    visited[rowNum][colNum]=true;
-                    q.add(new Pair(rowNum,colNum));
-                }
-            }
-        }
+
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
     }
 }
