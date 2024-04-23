43324e7f48bad1a7b24743c02baa5b7313f8efc1
Mitali Maurya <113610349+MitaliM13@users.noreply.github.com>
2024-04-23 19:17:24 +0530

    Time: 37 ms (32.76%) | Memory: 57.3 MB (29.87%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,41 +1,44 @@
-class Solution {
+import java.util.*;
+
+public class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if(n == 1) return Collections.singletonList(0);
-        
-        int ind[] = new int[n];
-        Map<Integer, List<Integer>> map = new HashMap();
-        for(int[] edge: edges) {
-            ind[edge[0]]++;
-            ind[edge[1]]++;
-            map.putIfAbsent(edge[0], new ArrayList());
-            map.putIfAbsent(edge[1], new ArrayList());
-            map.get(edge[0]).add(edge[1]);
-            map.get(edge[1]).add(edge[0]);
+        if (n == 1) {
+            return Collections.singletonList(0);
         }
 
-        Queue<Integer> queue = new LinkedList();
-        for(int i=0;i<ind.length;i++) {
-            if(ind[i] == 1) {
-                queue.add(i);
+        // Construct the adjacency list representation of the tree
+        List<Set<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new HashSet<>());
+        }
+        for (int[] edge : edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+
+        // Initialize a queue with all leaf nodes
+        Queue<Integer> leaves = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (adjList.get(i).size() == 1) {
+                leaves.offer(i);
             }
         }
 
-        int processed = 0;
-        while(n - processed > 2) {
-            int size = queue.size();
-            processed += size;
-            for(int i=0;i<size;i++) {
-                int poll = queue.poll();
-                for(int adj: map.get(poll)) {
-                    if(--ind[adj] == 1) {
-                        queue.add(adj);
-                    }
+        // Perform BFS to remove layers of nodes until one or two nodes remain
+        while (n > 2) {
+            int size = leaves.size();
+            n -= size;
+            for (int i = 0; i < size; i++) {
+                int leaf = leaves.poll();
+                int neighbor = adjList.get(leaf).iterator().next();
+                adjList.get(neighbor).remove(leaf);
+                if (adjList.get(neighbor).size() == 1) {
+                    leaves.offer(neighbor);
                 }
             }
         }
 
-        List<Integer> list = new ArrayList();
-        list.addAll(queue);
-        return list;
+        // The remaining nodes are the roots of the MHTs
+        return new ArrayList<>(leaves);
     }
 }
