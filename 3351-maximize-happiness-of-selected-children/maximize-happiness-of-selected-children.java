224e37b090090523e1620a750aefb52422209e18
Mitali Maurya <113610349+MitaliM13@users.noreply.github.com>
2024-05-09 10:13:51 +0530

    Time: 34 ms (97.44%) | Memory: 61.7 MB (43.21%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -1,11 +1,12 @@
 class Solution {
-    public long maximumHappinessSum(int[] h, int k) {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int dec = 0;
         long max=0;
-        int c=0,value=0;
-        Arrays.sort(h);
-        for(int i=h.length-1;i>=h.length-k;i--)
-        {
-            value = h[i]-c++;
+        int value=0;
+        for(int i=happiness.length-1;i>=happiness.length-k;i--){
+            value = happiness[i]-dec;
+            dec++;
             if(value>0)
             {
                 max += value;
